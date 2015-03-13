61fac18f0083cad628eb1a0b25dbd83df9b100fe
amy-cuevas <acuevas@kippnyccp.org>
2015-03-13 09:07:50 -0400

    Update Javascript.js

--- memory/Javascript.js
+++ memory/Javascript.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
   var secong_guess=0
   var first_card=0
   var card_array=["1","1","2","2","3","3","4","4","5","5","6","6","7","7","8","8","9","9","10","10"]
+  var shuffled_array = shuffle(card_array)
   for(i in card_array){
     $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
   }
