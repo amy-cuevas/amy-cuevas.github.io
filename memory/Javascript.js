3f69da6665e2be7eff262dde48585a289a6e68d1
amy-cuevas <acuevas@kippnyccp.org>
2015-03-13 09:06:03 -0400

    Update Javascript.js

--- memory/Javascript.js
+++ memory/Javascript.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
-  ["1","2","3","4","5","6","7","8","9","10","1","2","3","4","5","6","7","8","9","10"]
+  var click_count=0
+  var first_guess=0
+  var secong_guess=0
+  var first_card=0
+  var card_array=["1","1","2","2","3","3","4","4","5","5","6","6","7","7","8","8","9","9","10","10"]
   for(i in card_array){
     $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
   }
