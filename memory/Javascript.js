2b9242e1a6ef37bb807a16f0157edabe1fdfe682
amy-cuevas <acuevas@kippnyccp.org>
2015-03-13 08:44:40 -0400

    Update Javascript.js

--- memory/Javascript.js
+++ memory/Javascript.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
-  for(i=1; i<21; i++){
-    $('#card_holder').append('<div class="card"><p>'+i+'</p></div>');
+  ["1","2","3","4","5","6","7","8","9","10","1","2","3","4","5","6","7","8","9","10"]
+  for(i in card_array){
+    $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
   }
   $("div.card").click(function(){
     var mynum=0;
