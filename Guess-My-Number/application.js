453fadd8c7ce733ccd44de2e6ac27c9d49662f70
amy-cuevas <acuevas@kippnyccp.org>
2015-04-01 08:40:10 -0400

    Update application.js

--- Guess-My-Number/application.js
+++ Guess-My-Number/application.js
@@ -5,28 +5,30 @@ $(document).ready(function(){
   //when the player clicks on the 'guess' button
   $('button').on('click', function(){
     //save their guess as a variable
-    var userGuess=$('input').val()
+    var userGuess=parseInt $('input').val();
     
     
     //and compare this guess to the random number that the computer picked
     //if the user guessed the correct number...
-    if userGuess===random_Integer
+    if (userGuess===random_Integer){
     
       //what happens if the guess is correct?
-      confirm("Congratulations!You guessed correctly!")
+      confirm("Congratulations!You guessed correctly!");
+    }
       
     //if the user guessed too high...
-    else if random_Integer<userGuess
+    else if (random_Integer<userGuess){
     
       //update the 'feedback' paragraph to tell them to guess lower
-      console.log("Guess lower")
+      console.log("Guess lower");
+    }
       
     //otherwise, the user guessed too low...
-    else if random_Integer<userGuess
+    else if (random_Integer<userGuess){
     
     
     
       //update the 'feedback' paragraph to tell them to guess higher
-    console.log("Guess higer")
+    console.log("Guess higer");}
   });
 });
