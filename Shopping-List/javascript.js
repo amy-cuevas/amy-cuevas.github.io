163f8ea62375d3a6e54bc79625b5dd0faeb2b508
amy-cuevas <acuevas@kippnyccp.org>
2015-04-16 08:49:28 -0400

    Update javascript.js

--- Shopping-List/javascript.js
+++ Shopping-List/javascript.js
@@ -1,1 +1,12 @@
+$(document).ready(function(){
+  $('#add').on('click',function()){
+    var item=$('input').val();
+    $("div").append('<p>'+item+'</p>')
+  });
+$('#remove').on('click',function()){
+    var item=$('input').val();
+    $('div').empty();
+})
+}
+}
 
