4c893efb9e71e3d0d89647884db36e581f4769bb
amy-cuevas <acuevas@kippnyccp.org>
2015-04-17 08:43:04 -0400

    Update Javascript.js

--- memory/Javascript.js
+++ memory/Javascript.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 var click_count=0;
  var first_guess=0;
  var second_guess=0;
- var first_card=0;
+ var first_card=0;//all these allow for the game to restart
  var card_array = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "10", "10"]
 
 function shuffle(array) {
